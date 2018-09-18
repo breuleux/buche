a0015bb12765ab50431f69958515a8486a0d0144
Olivier Breuleux <breuleux@gmail.com>
2018-09-18 12:04:42 -0400

    Add nicer-repl.py

--- examples/pyrepl/README.md
+++ examples/pyrepl/README.md
@@ -5,10 +5,27 @@ The `<buche-input>` element can be used to read input from the user inside the B
 
 This can be used to write, among other things, REPLs for various languages. Here we have one for Python.
 
-## Run the example:
+## Run the examples:
+
+There are two examples in this directory. `repl.py` has no dependencies, whereas `nicer-repl.py` depends on the `buche` package for Python.
+
+### No dependencies
 
     buche --inspect python3 -u repl.py
 
 The `-u` flag is important to use here, because otherwise Python will buffer `stdin` from Buche and hang (it knows that it is not reading from the terminal and therefore (wrongly) guesses that we are not in an interactive session).
 
 Also, this doesn't work in Python 2. I don't know why. Don't use Python 2, it is old and rotten.
+
+### Buche dependency
+
+The [buche](https://github.com/breuleux/pybuche) package for Python provides:
+
+* A nicer interface.
+* HTML pretty-printing for all objects.
+* An event-based reader for events coming from Buche on stdin.
+
+    pip install buche
+    buche --inspect python3 -u nicer-repl.py
+
+Notice that arrays and dictionaries are pretty-printed, and you can even click on an object to stash it in a variable!
---
+++ examples/pyrepl/nicer-repl.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env buche --inspect python3 -u
+
+# Python interactive interpreter. Requires the buche package and
+# Python >=3.6
+
+import json
+import sys
+try:
+    from buche import buche, reader
+except ImportError:
+    print('This demo requires the buche package (pip install buche)',
+          file=sys.stderr)
+    sys.exit(1)
+
+_ = None
+
+buche.html.style('''
+.repl-container { height: 100%; width: 100%;
+                  display: flex; flex-direction: column; }
+log-entry.echo, buche-input > input { font-family: monospace; }
+buche-input > input { font-family: monospace; font-size: 16px; }
+''')
+
+container = buche.open.div['repl-container']()
+repl = container.open.bucheLog()
+inp = container.open.bucheInput()
+
+repl.html.b('Enter Python code in the input box at the bottom!')
+repl.html.b('Try clicking on results :)')
+inp.command_focus()
+
+code_globals = globals()
+
+@reader.on_submit
+def repl_line(event, message):
+    code = message.value
+    if code.strip() == '':
+        return
+    repl.html.logEntry['echo'](code)
+    inp.command_set()
+    try:
+        try:
+            res = eval(code, code_globals)
+            if res is not None:
+                repl.repr.logEntry['result'](res, interactive=True)
+        except SyntaxError:
+            exec(code, code_globals)
+    except Exception as exc:
+        repl.repr.logEntry['error'](exc)
+
+@reader.on_click
+def repl_select(event, message):
+    global _
+    if hasattr(message, 'obj'):
+        _ = message.obj
+        repl.html.logEntry['echo'](f'_ = {message.obj}')
+    inp.command_focus()
+
+reader.start()
