30c621853a62984a20f23c31e8eebff254ef5c21
Olivier Breuleux <breuleux@gmail.com>
2018-09-25 22:29:38 -0400

    Update graph example

---
+++ examples/graph/README.md
@@ -0,0 +1,12 @@
+
+# Graphs
+
+Graphs can be displayed using the `buche-cytoscape` plugin. That plugin is not installed by default. You can install it via command line as follows:
+
+    buche --install cytoscape
+
+Alternatively, you can just run the example and install the plugin from the GUI.
+
+## Run the example:
+
+    buche -v node graph.js
---
+++ examples/graph/graph.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env buche -v node
+
+// Neat graphs.
+
+function buche(cfg) {
+    console.log(JSON.stringify(cfg));
+}
+
+buche({
+    command: 'plugin',
+    name: 'cytoscape'
+});
+
+buche({
+    parent: "/",
+    tag: 'cytoscape-graph',
+    attributes: {
+        address: 'graph',
+        width: '1000px',
+        height: '1000px',
+    }
+})
+
+buche({
+    parent: "/graph",
+    command: "configure",
+    style: `${__dirname}/graph-style.css`
+})
+
+let connList = process.argv.slice(2);
+if (connList.length == 0) {
+    connList = 'AB BC CA AD DE'.split(' ');
+}
+
+let connections = connList.map(pair => pair.split(''));
+
+for (let [from, to] of connections) {
+    buche({
+        command: 'element',
+        parent: '/graph',
+        data: {
+            source: from,
+            target: to
+        }
+    });
+}
