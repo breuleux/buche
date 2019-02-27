1c8e043d5f20ddc4b09c454de886b7b3d6270620
Olivier Breuleux <breuleux@gmail.com>
2019-02-27 13:31:59 -0500

    Update nicer-repl

--- examples/pyrepl/nicer-repl.py
+++ examples/pyrepl/nicer-repl.py
@@ -1,59 +1,36 @@
-#!/usr/bin/env buche --inspect python3 -u
+#!/usr/bin/env buche -v python3 -u
 
 # Python interactive interpreter. Requires the buche package and
 # Python >=3.6
 
-import json
-import sys
 try:
-    from buche import buche, reader
+    from buche import buche, reader, Repl
 except ImportError:
-    print('This demo requires the buche package (pip install buche)',
+    print('This demo requires the Python buche package (pip install buche)',
           file=sys.stderr)
     sys.exit(1)
 
-_ = None
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
 
-buche.html.style('''
-.repl-container { height: 100%; width: 100%;
-                  display: flex; flex-direction: column; }
-log-entry.echo, buche-input > input { font-family: monospace; }
-buche-input > input { font-family: monospace; font-size: 16px; }
-''')
+    def __hrepr__(self, H, hrepr):
+        sz = hrepr.config.swatch_size or 20
+        return H.div(
+            style=f'display:inline-block;width:{sz}px;height:{sz}px;margin:2px;'
+                  f'background-color:rgb({self.r},{self.g},{self.b});'
+        )
 
-container = buche.open.div['repl-container']()
-repl = container.open.bucheLog()
-inp = container.open.bucheInput()
+repl = Repl(buche, reader, code_globals=globals())
+repl.start()
+repl.log.html("""
+    <p><b>Enter Python expressions in the input box at the bottom.</b></p>
 
-repl.html.b('Enter Python code in the input box at the bottom!')
-repl.html.b('Try clicking on results :)')
-inp.command_focus()
+    <p>You can click on any part of the output to put it in a temporary
+    variable.</p>
 
-code_globals = globals()
-
-@reader.on_submit
-def repl_line(event, message):
-    code = message.value
-    if code.strip() == '':
-        return
-    repl.html.logEntry['echo'](code)
-    inp.command_set()
-    try:
-        try:
-            res = eval(code, code_globals)
-            if res is not None:
-                repl.show.logEntry['result'](res, interactive=True)
-        except SyntaxError:
-            exec(code, code_globals)
-    except Exception as exc:
-        repl.show.logEntry['error'](exc)
-
-@reader.on_click
-def repl_select(event, message):
-    global _
-    if hasattr(message, 'obj'):
-        _ = message.obj
-        repl.html.logEntry['echo'](f'_ = {message.obj}')
-    inp.command_focus()
-
-reader.start()
+    <p>Something to get you started:</p>
+""")
+repl.run('[Color(0, 0, i * 16) for i in range(16)]')
