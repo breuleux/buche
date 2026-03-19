4295c97ea3beee0cc1ebe722043cd7507a643b1c
Olivier Breuleux <breuleux@gmail.com>
2026-03-19 14:44:48 -0400

    WIP shell

--- src/shell.js
+++ src/shell.js
@@ -1,18 +1,59 @@
 const { spawn } = require("child_process");
 const { randomUUID } = require("crypto");
 
-class Shell {
-    async *run(inputStream) {
-        for await (const obj of inputStream) {
-            const handler = this[`handle$${obj.command}`];
-            if (handler) {
-                yield* handler.call(this, obj);
-            }
+async function* merge(iterables) {
+    const queue = [];
+    let resolve = null;
+    let active = 0;
+
+    function push(value) {
+        queue.push(value);
+        if (resolve) { const r = resolve; resolve = null; r(); }
+    }
+
+    function pump(iter) {
+        active++;
+        async function drain() {
+            for await (const value of iter) push({ value });
+            if (--active === 0) push({ done: true });
+            else push({ noop: true });
+        }
+        drain();
+    }
+
+    for await (const iter of iterables) {
+        pump(iter);
+        while (queue.length > 0) {
+            const item = queue.shift();
+            if (item.done) return;
+            if (!item.noop) yield item.value;
         }
     }
 
+    while (active > 0) {
+        while (queue.length > 0) {
+            const item = queue.shift();
+            if (item.done) return;
+            if (!item.noop) yield item.value;
+        }
+        await new Promise((r) => { resolve = r; });
+    }
+}
+
+class Shell {
+    run(inputStream) {
+        const self = this;
+        async function* handlers() {
+            for await (const obj of inputStream) {
+                const handler = self[`handle$${obj.command}`];
+                if (handler) yield handler.call(self, obj);
+            }
+        }
+        return merge(handlers());
+    }
+
     async *handle$run(obj) {
-        const id = randomUUID();
+        const id = obj.id ?? randomUUID();
         yield { type: "process_start", id };
 
         const [cmd, ...args] = [obj.function, ...(obj.args || [])];
@@ -24,11 +65,7 @@ class Shell {
 
         function push(value) {
             events.push(value);
-            if (resolve) {
-                const r = resolve;
-                resolve = null;
-                r();
-            }
+            if (resolve) { const r = resolve; resolve = null; r(); }
         }
 
         child.stdout.on("data", (data) => push({ type: "stdout", id, data: data.toString() }));
